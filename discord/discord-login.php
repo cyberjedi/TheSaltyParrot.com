f07fe53d114b4c1c0c328310090727a0413f07af
cyberjedi <stuartgreenwell@icloud.com>
2025-03-08 09:10:47 -0700

    Update discord-login.php

--- discord/discord-login.php
+++ discord/discord-login.php
@@ -2,9 +2,6 @@
 // File: discord/discord-login.php
 require_once 'discord-config.php';
 
-// Store the referring page to redirect back after authentication
-$_SESSION['discord_return_url'] = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '../index.php';
-
 // Generate a random state parameter to prevent CSRF attacks
 $state = bin2hex(random_bytes(16));
 $_SESSION['discord_oauth_state'] = $state;
@@ -16,31 +13,19 @@ $auth_url .= '&redirect_uri=' . urlencode(DISCORD_REDIRECT_URI);
 $auth_url .= '&response_type=code';
 $auth_url .= '&state=' . $state;
 $auth_url .= '&scope=identify%20guilds%20guilds.members.read%20guilds.channels.read';
-
-// Open in a popup window instead of redirecting
-echo '<!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html>
 <head>
-    <title>Discord Authentication</title>
+    <title>Discord Login</title>
+    <script>
+        window.open('<?php echo $auth_url; ?>', 'discord-login', 'width=600,height=800');
+        setTimeout(function() {
+            window.location.href = '../index.php';
+        }, 500);
+    </script>
 </head>
 <body>
-    <script>
-        var popup = window.open("' . $auth_url . '", "discord-oauth", "width=600,height=800");
-        if (!popup || popup.closed || typeof popup.closed==\'undefined\') {
-            // Popup blocked, try direct redirect
-            window.location.href = "' . $auth_url . '";
-        } else {
-            // Wait for the popup to be closed, then redirect
-            var timer = setInterval(function() {
-                if(popup.closed) {
-                    clearInterval(timer);
-                    window.location.href = "../index.php";
-                }
-            }, 1000);
-        }
-    </script>
-    <p>Authenticating with Discord... If this page doesn\'t close automatically, <a href="../index.php">click here</a> to return.</p>
+    <p>Opening Discord authentication... If nothing happens, <a href="<?php echo $auth_url; ?>" target="_blank">click here</a>.</p>
 </body>
-</html>';
-exit;
-?>
+</html>
