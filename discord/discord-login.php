00986f501ff5bfd8b01062a61d2bd1f3928aa87f
cyberjedi <stuartgreenwell@icloud.com>
2025-03-07 21:54:52 -0700

    Update discord-login.php

--- discord/discord-login.php
+++ discord/discord-login.php
@@ -8,14 +8,13 @@ require_once 'discord-config.php';
 $state = bin2hex(random_bytes(16));
 $_SESSION['discord_oauth_state'] = $state;
 
-// Build the authorization URL with just the basic scopes needed for login
-// We only need identify and guilds for authentication
+// Build the authorization URL with all necessary scopes
 $auth_url = DISCORD_API_URL . '/oauth2/authorize';
 $auth_url .= '?client_id=' . DISCORD_CLIENT_ID;
 $auth_url .= '&redirect_uri=' . urlencode(DISCORD_REDIRECT_URI);
 $auth_url .= '&response_type=code';
 $auth_url .= '&state=' . $state;
-$auth_url .= '&scope=identify%20guilds';
+$auth_url .= '&scope=identify%20guilds%20guilds.channels.read';
 
 // Redirect the user to Discord's authorization page
 header('Location: ' . $auth_url);
