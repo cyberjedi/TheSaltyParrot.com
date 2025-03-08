64ec4adbd190a03f819ce756914007d6244e5774
cyberjedi <stuartgreenwell@icloud.com>
2025-03-08 08:46:05 -0700

    Update discord-login.php

--- discord/discord-login.php
+++ discord/discord-login.php
@@ -1,8 +1,9 @@
 <?php
 // File: discord/discord-login.php
-// This file initiates the Discord OAuth process
-
 require_once 'discord-config.php';
+
+// Store the referring page to redirect back after authentication
+$_SESSION['discord_return_url'] = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '../index.php';
 
 // Generate a random state parameter to prevent CSRF attacks
 $state = bin2hex(random_bytes(16));
@@ -14,7 +15,7 @@ $auth_url .= '?client_id=' . DISCORD_CLIENT_ID;
 $auth_url .= '&redirect_uri=' . urlencode(DISCORD_REDIRECT_URI);
 $auth_url .= '&response_type=code';
 $auth_url .= '&state=' . $state;
-$auth_url .= '&scope=identify%20guilds%20guilds.channels.read';
+$auth_url .= '&scope=identify%20guilds%20guilds.members.read%20guilds.channels.read';
 
 // Redirect the user to Discord's authorization page
 header('Location: ' . $auth_url);
