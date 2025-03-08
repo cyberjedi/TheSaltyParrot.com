cac5c5251c4e2ff105803a043171a185e9b71c82
cyberjedi <stuartgreenwell@icloud.com>
2025-03-07 22:16:32 -0700

    Update reauth.php

--- discord/reauth.php
+++ discord/reauth.php
@@ -1,6 +1,6 @@
 <?php
 // File: discord/reauth.php
-// This file forces Discord re-authentication with proper scopes
+// This file handles Discord re-authentication with proper scopes
 
 require_once 'discord-config.php';
 
@@ -17,13 +17,17 @@ $_SESSION['discord_message'] = 'You have been logged out of Discord to refresh y
 $state = bin2hex(random_bytes(16));
 $_SESSION['discord_oauth_state'] = $state;
 
-// Build the authorization URL with all the scopes we need for webhook management
+// Build the authorization URL with all necessary scopes
+// We explicitly include the required scopes for proper access
 $auth_url = DISCORD_API_URL . '/oauth2/authorize';
 $auth_url .= '?client_id=' . DISCORD_CLIENT_ID;
 $auth_url .= '&redirect_uri=' . urlencode(DISCORD_REDIRECT_URI);
 $auth_url .= '&response_type=code';
 $auth_url .= '&state=' . $state;
-$auth_url .= '&scope=identify%20guilds%20guilds.channels.read'; // Now includes guilds.channels.read
+$auth_url .= '&scope=' . urlencode('identify guilds guilds.members.read');
+
+// Log the reauth attempt
+error_log('Discord reauth initiated. Redirect URL: ' . $auth_url);
 
 // Redirect the user to Discord's authorization page
 header('Location: ' . $auth_url);
